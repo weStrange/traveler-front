f041c4725de071658d68b00871331ea7773c2f63
Aleksandr Tereshchenko <terap98@icloud.com>
2017-04-23 14:26:44 +0300

    Added dependencies

--- src/card-queue/types.js
+++ src/card-queue/types.js
@@ -1,2 +1,30 @@
 /* @flow */
 'use strict'
+
+import { List } from 'immutable'
+
+import type { Profile } from '../core/types'
+
+export type PersonalCard = {
+  id: number,
+  startTime: number,
+  endTime: number,
+  lon: number,
+  lat: number,
+  owner: Profile
+}
+
+export type GroupCard = {
+  id: number,
+  startTime: number,
+  endTime: number,
+  lon: number,
+  lat: number,
+  owner: Profile,
+  participants: List<string>
+}
+
+export type CardQueueState = {
+  personalCards: List<PersonalCard>,
+  groupCards: List<GroupCard>
+}
